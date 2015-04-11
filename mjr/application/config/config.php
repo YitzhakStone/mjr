6271bdcbdb90a7e09510a23d611b9065bc67d71a
robsonpiere <robsonpiere@outlook.com>
2015-04-11 18:12:01 -0300

    teste
    testes

--- mjr/application/config/config.php
+++ mjr/application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://localhost/mjr/';
+$config['base_url']	= 'http://localhost/mjr/mjr';
 
 /*
 |--------------------------------------------------------------------------
